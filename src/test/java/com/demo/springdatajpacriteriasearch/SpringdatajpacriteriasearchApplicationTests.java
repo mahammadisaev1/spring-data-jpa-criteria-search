ab50d062b962d8dce10b8f9e3467630a739d3101
mahammadisaev1 <mahammadisaev1@gmail.com>
2024-10-17 14:47:56 +0400

    intialize project

---
+++ src/test/java/com/demo/springdatajpacriteriasearch/SpringdatajpacriteriasearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springdatajpacriteriasearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdatajpacriteriasearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
